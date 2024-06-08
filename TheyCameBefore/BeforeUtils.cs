6eae4e80ffc850329f59a05b5b94e4a60c71f2bb
Artur Martins <arturjcmartins.dev@gmail.com>
2024-06-09 00:51:16 +0100

    Small commit with space

--- TheyCameBefore/BeforeUtils.cs
+++ TheyCameBefore/BeforeUtils.cs
@@ -7,8 +7,8 @@ namespace TheyCameBefore
 {
     public class BeforeUtils
     {
-        public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items, 
-        T item) where T : struct, IComparable<T>
+        public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items,
+         T item) where T : struct, IComparable<T>
         {
             return items.Where(x => x.CompareTo(item) < 0);
         }
